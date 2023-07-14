056a15f33ff3f11d5b97ec91ccf0f0331d9c69fc
Daouda Warme <dwarme99@gmail.com>
2023-07-14 23:27:38 +0200

    graphql resolvers - signUp

--- src/graphql/auth-gql/auth-resolvers.ts
+++ src/graphql/auth-gql/auth-resolvers.ts
@@ -5,15 +5,26 @@ import { removeTokenCookie } from "../../utils/auth-cookies-util";
 
 const authResolvers = {
     Mutation: {
-        async signUp(_parent: any, args: { input: IUserCreateInput }, _context: any, _info: any) {
+        async signUp(_parent: any, args: { input: IUserCreateInput }, context: any, _info: any) {
+            try {
 
+                const { user } = await apiController.signUp(context.req, context.res, args.input)
+                return { user }
+
+            } catch (error: any) {
+                if ('object' in error && error.object == 'iclofy_error') {
+                    throw graphQLError(error.message, {}, error)
+                }
+
+                throw graphQLErrorUnknow
+            }
         },
 
         async signIn(_parent: any, args: { input: IAuthLoginInput }, context: any, _info: any) {
             try {
 
-                const {user} = await apiController.signIn(context.req, context.res, args.input)
-                return {user}
+                const { user } = await apiController.signIn(context.req, context.res, args.input)
+                return { user }
 
             } catch (error: any) {
                 if ('object' in error && error.object == 'iclofy_error') {
